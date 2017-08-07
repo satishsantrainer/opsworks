864d31323bad139d3bdbb5a9951e38955290edb2
satishsantrainer <satish.karlapudi@gmail.com>
2017-08-06 20:42:25 -0400

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
     <body>
 <div align="center">
-                <h1>Welcome to Online Flower Shop</h1>
+                <h1>Welcome to Online Flower Shop in US</h1>
                 <h2>Congratulations! You have successfully installed an application using OpsWorks!</h2>
                 <div align="center"><img src="https://s3.amazonaws.com/bucket098/flowers.jpg"></div>
                 <p>Your Application is now running on the host &ldquo;<?php echo gethostname(); ?>&rdquo; in a new stack and layer in OpsWorks.</p>
